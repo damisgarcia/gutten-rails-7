484eb7263c59f69fcd6baee453cd0ff72c008049
Damis Garcia <damisgarcia@gmail.com>
2023-09-11 11:41:16 -0300

    Adicioando Imagem para Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@
 
 class Post < ApplicationRecord
   has_rich_text :body
+
+  has_one_attached :media do |attachable|
+    attachable.variant :thumb, resize_to_limit: [300, 300]
+    attachable.variant :medium, resize_to_limit: [600, 600]
+  end
 end
