e940f9f6dbbf2222065c4ed10905b5aef58fc073
Damis Garcia <damisgarcia@gmail.com>
2024-04-12 16:54:00 -0300

    configurado tailwindcss para dividir css application, admin

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -20,7 +20,6 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-    require('@tailwindcss/typography'),
     require("daisyui")
   ],
   daisyui: {
